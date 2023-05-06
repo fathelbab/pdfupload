0ba2b7b2346f84071da768424b61e3ce5c17d648
mohamed ahmed fathelbab <fathelbabmohamed94@gmail.com>
2023-05-06 19:35:18 +0300

    rack session fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
      sign_in: 'login', 
-     sign_out: 'logout' 
+     sign_out: 'logout', 
+     registration: 'signup'
      },
       controllers: { 
       sessions: 'users/sessions',
